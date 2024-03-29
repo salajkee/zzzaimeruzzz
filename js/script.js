b04588fe65fd8267903699acb413900d8f89cd33
Salamat <salajkee@mail.ru>
2024-03-29 16:53:18 +0500

    qweqewq

--- js/script.js
+++ js/script.js
@@ -385,19 +385,19 @@ try {
 	const valueDisplays = document.querySelectorAll('.num');
 	const interval = 3000;
 
-	// valueDisplays.forEach(valueDisplay => {
-	// 	let startValue = 0;
-	// 	let endValue = parseInt(valueDisplay.getAttribute('data-val'));
+	valueDisplays.forEach(valueDisplay => {
+		let startValue = 0;
+		let endValue = parseInt(valueDisplay.getAttribute('data-val'));
 
-	// 	let duration = Math.floor(interval / endValue);
-	// 	let counter = setInterval(() => {
-	// 		startValue += 1;
-	// 		valueDisplay.textContent = startValue;
-	// 		if (startValue == endValue) {
-	// 			clearInterval(counter);
-	// 		}
-	// 	}, duration);
-	// });
+		let duration = Math.floor(interval / endValue);
+		let counter = setInterval(() => {
+			startValue += 1;
+			valueDisplay.textContent = startValue;
+			if (startValue == endValue) {
+				clearInterval(counter);
+			}
+		}, duration);
+	});
 } catch (e) {}
 
 // Faq accordion
